7bafeceeaa78f767089a5b72164ea48805164fc9
Zagaw <myathtikeaung745@gmail.com>
2026-08-04 21:49:39 +0630

    first commit

---
+++ src/test/java/com/example/projectCollab/ProjectCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectCollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
